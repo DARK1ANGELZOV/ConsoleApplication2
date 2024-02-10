75e7e584e1212cb0f202a67feb832caf295ffdb8
Dark_Angel <Dark_Angel@DESKTOP-15E22GB>
2024-02-10 18:29:50 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int lineyka(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Ключ найден!" << endl;
+            return i;
+        }
+
+    }
+    return -1;
+}
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    cout << "Массив: " << endl;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Ключ: " << endl;
+    int key;
+    cin >> key;
+    int result = lineyka(arr, n, key);
+
+    if (result == -1) {
+        cout << "Ключ не найден!" << endl;
+    }
+    else {
+        cout << "Ключ найден: " << result << endl;
+    }
+
+    delete[] arr;
+}
+
